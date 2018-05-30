743ca36e1e63f8d55f3a952443b4f59a426451a6
Gunter Grodotzki <gunter@grodotzki.co.za>
2018-05-30 17:53:27 +0200

    fix publish script (#2)

--- publish.sh
+++ publish.sh
@@ -15,7 +15,7 @@ trap 'throw_exception' ERR
 
 for folder in targets/*; do
   consolelog "going into: ${folder}"
-  IFS='/' read -r -a paths <<< "${name}"
+  IFS='/' read -r -a paths <<< "${folder}"
 
   for deb in "${folder}/"*.deb; do
     filename="${deb##*/}"
@@ -28,6 +28,7 @@ for folder in targets/*; do
       -X PUT \
       -T "${deb}" \
       -u "${BINTRAY_USER}:${BINTRAY_API_KEY}" \
+      -f \
       "https://api.bintray.com/content/${BINTRAY_PROJECT}/${tags[0]}/${tags[1]}/debian/${paths[1]}/${filename};deb_distribution=${paths[1]};deb_component=main;deb_architecture=${tags[2]};publish=1"
   done
 done
